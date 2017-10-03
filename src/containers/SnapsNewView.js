ce79be45df94996140c4799a01e1cbb170d7cfb1
Eva Raymond <evathecoderr@outlook.com>
2017-10-03 10:59:14 +0100

    Scaffolded view to add new snaps

---
+++ src/containers/SnapsNewView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import App from 'grommet/components/App'
+
+class SnapsNewView extends Component {
+    render() {
+        return (
+            <App>Snaps View New </App>
+        );
+    }
+}
+
+export default SnapsNewView
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import SnapsView from '../containers/SnapsView'
+import SnapsNewView from '../containers/SnapsNewView'
 
 const Router = () => {
     return (
         <div>
-            <Route path="/" component={SnapsView}></Route>
+            <Switch>
+                <Route path="/" exact component={SnapsView}></Route>
+                <Route path="/snaps/new" component={SnapsNewView}></Route>
+            </Switch>
             {/* <Route path="" component={}></Route>
-            <Route path="" component={}></Route>
             <Route path="" component={}></Route>
             <Route path="" component={}></Route> */}
         </div>
