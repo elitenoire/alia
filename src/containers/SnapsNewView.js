db62e54472467993d80789990759f4bc4763396f
Eva Raymond <evathecoderr@outlook.com>
2017-10-06 20:54:35 +0100

    redux form binding

--- src/containers/SnapsNewView.js
+++ src/containers/SnapsNewView.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
 import App from 'grommet/components/App'
+import Article from 'grommet/components/Article'
+import Heading from 'grommet/components/Heading'
+import Section from 'grommet/components/Section'
+import Footer from 'grommet/components/Footer'
+import Form from 'grommet/components/Form'
+import FormFields from 'grommet/components/FormFields'
+import TextInputWithFormField from 'redux-form-binding-grommet/src/TextInput'
+// import TextInput from 'grommet/components/TextInput'
 
 class SnapsNewView extends Component {
+    // renderTitleField = ({ input }) => {
+    //     return (
+    //         <TextInput
+    //         onDOMChange
+
+    //         />
+    //     )
+    // }
     render() {
         return (
-            <App>Snaps View New </App>
+            <App>
+                <Form>
+                    <Article>
+                        <Heading>Snaps View New</Heading>
+                        <Section>
+                            <FormFields>
+                                <Field name="title" label="Title" id="title-1" placeholder="Snap Title"
+                                    component={TextInputWithFormField} />
+
+                            </FormFields>
+                        </Section>
+                        <Footer></Footer>
+                    </Article>
+                </Form>
+            </App>
         );
     }
 }
 
-export default SnapsNewView
+export default reduxForm({form:'NewSnapForm'})(SnapsNewView)
