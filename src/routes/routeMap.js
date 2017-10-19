a0875704b6ce9f118586299fd2aff4703aa5c972
Eva Raymond <evathecoderr@outlook.com>
2017-10-19 14:16:11 +0100

    fixed routing for deployment

--- src/routes/routeMap.js
+++ src/routes/routeMap.js
@@ -1,8 +1,8 @@
 //PATHS
-export const HOME_PATH = '/'
-export const CREATE_SNAP_PATH = '/snaps/new'
-export const EDIT_SNAP_PATH = '/snaps/edit/'
-export const VIEW_SNAP_PATH = '/snaps/'
+export const HOME_PATH = process.env.PUBLIC_URL + '/'
+export const CREATE_SNAP_PATH = process.env.PUBLIC_URL + '/snaps/new'
+export const EDIT_SNAP_PATH = process.env.PUBLIC_URL + '/snaps/edit/'
+export const VIEW_SNAP_PATH = process.env.PUBLIC_URL + '/snaps/'
 
 export default {
     HOME_PATH,
